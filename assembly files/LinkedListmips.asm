2dffd31a2e87626553398bba9c0171601dbfbc00
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-02-17 12:45:06 +0530

    Add files via upload

--- assembly files/LinkedListmips.asm
+++ assembly files/LinkedListmips.asm
@@ -9,7 +9,7 @@
     .asciiz "Enter the size of the Linked List:\n"
   
   promptEdge:
-    .asciiz "Enter the connections:\n"
+    .asciiz "\nEnter the connections:\n\n"
     
   promptTo:
     .asciiz "To node :  "
@@ -18,7 +18,7 @@
     .asciiz "From node :  "
     
   promptCheck:
-    .asciiz "Checking for cycles now."
+    .asciiz "\n\nChecking for cycles now:"
     
   promptNoC:
     .asciiz "\nNo cycles found.\n\nPrinting the nodes:\n"
@@ -27,7 +27,7 @@
     .asciiz "\nA cycle was found after node "
     
   promptBreak:  
-    .asciiz "\n\nBreaking the cycle now.\n\nPrinting the nodes:\n"
+    .asciiz "\n\nThe cycle has been broken now.\n\nPrinting the nodes:\n"
     
   nl:
     .asciiz "\n"
@@ -36,16 +36,24 @@
 .text
   main:
     jal makeLL            # Calling a procedure to prepare a linked list from user-input
+    
+    li $v0, 4
+    la $a0, promptCheck    # Displays it to the user
+    syscall
+    
     lw $a0, head          # Readying arguments for next procedure
     jal FloydCycleFind    # Checking for cycles
+    
     beq $v1, $0, nocycle
+    
     move $a0, $v0         # Readying arguments for next procedure
     lw $a1, head          #    "         "      "    "      "
-    jal FloydCycleNode    # Finds the node where the cycle starts
+    jal FloydCycleBreak   # Finds the node where the cycle starts
     
     li $v0, 4
     la $a0, promptYesC    # Displays it to the user
     syscall
+    
     move $a0, $v1         # Printing value of node to be connected to NULL 
     li $v0, 1 
     syscall 
@@ -54,8 +62,8 @@
     la $a0, promptBreak    # Starts to break the cycle
     syscall 
     
-    move $a0, $v1          # Readying arguments for next procedure        
-    jal BreakCyclePrint    # Breaking cycle and printing the value too
+    la $a0, head           # Readying arguments for next procedure      
+    jal printLL		    # Breaking cycle and printing the value too
     j endProg              # Procedure to end program
     
     
@@ -64,6 +72,7 @@
     li $v0, 4
     la $a0, promptNoC      # Displays to the user
     syscall 
+    
     la $a0, head           # Readying arguments for next procedure
     jal printLL            # Calling a procedure to print linked-list
     j endProg              # Procedure to end program
@@ -132,6 +141,10 @@
       syscall 
       move $t3, $v0
       
+      li $v0, 4
+      la $a0, nl
+      syscall
+      
       sll $t2, $t2, 3
       addiu $t2, $t2, 4 # Preparing pointer to faddress
       add $t2, $t2, $t0
@@ -182,65 +195,46 @@
 
   FloydCycleFind:
   
-    lw $t0, 4($a0)               # fp = (head);
-    lw $t1, 4($a0)               # sp = (head);
+    move $t0, $a0               # fp = (head);
+    move $t1, $a0               # sp = (head);
+    move $t2, $a0
         
     startFindLoop:
       beq $t0, $0, exitFindLoop  # while (fp != NULL){
       lw $t0, 4($t0)             # fp = fp -> next;
-      beq $t0, $0, exitFindLoop  # if (fp == NULL) break;               
+      beq $t0, $0, exitFindLoop  # if (fp == NULL) break;
+      move $t2, $t1           
       lw $t1, 4($t1)             # sp = sp -> next;
       lw $t0, 4($t0)             # fp = fp -> next;
       beq $t0, $t1, exitFindLoop # if (fp == sp) break;
       j startFindLoop            # }
       
     exitFindLoop:
+      move $a3, $t2
       move $v0, $t1
       seq  $v1, $t0, $t1
       jr $ra
   
   
-  FloydCycleNode:
+  FloydCycleBreak:
     
-    lw $t0, 4($a0)			# p1 = fp -> next;
-    lw $t1, 4($a1)			# p2 = head;
-    lw $t1, 4($t1)			# p2 = p2 -> next;
-    lw $t2, 0($a0)			# val = fp -> value;
+    move $t0, $a0			# p1 = fp;
+    move $t1, $a1			# p2 = head;
+    move $t3, $a3			# p3 = before(fp);
+    lw $t2, 0($a3)#a0			# val = fp -> value;
     
     startNodeLoop:
     	beq $t0, $t1, exitNodeLoop	# while(p1 != p2){
+    	move $t3, $t0
     	lw $t2, 0($t0)                 # val = p1 -> val;
     	lw $t0, 4($t0)			# p1 = p1 -> next;
     	lw $t1, 4($t1)			# p2 = p2 -> next;
     	j startNodeLoop			# }
     	
     exitNodeLoop:
+    	sw $0, 4($t3)
     	move $v1, $t2			# return val;  	    
     	jr $ra
-  
-  
-  BreakCyclePrint:
-
-    move $a1, $ra
-    
-    lw $t0, head			# p1 = head -> next;
-    lw $t0, 4($t0)
-    lw $t1, head			# val = head -> value;
-    lw $t1, 0($t1)
-    move $t3, $a0			# breakPoint = argument;
-        
-    startBreakLoop:
-    	move $a0, $t1
-    	jal printInt			# print(val)
-        beq $t1, $t3, endBreakLoop	# while (val != breakPoint){
-        move $t2, $t0			# p2 = p1;
-        lw $t1, 0($t0)			# val = p1 -> value;
-        lw $t0, 4($t0)			# p1 = p1 -> next;
-	j startBreakLoop		# }
-	
-    endBreakLoop:
-    	sw $0, 4($t2)
-    	jr $a1
 
   endProg: #This is to exit the program
     li $v0, 10 
