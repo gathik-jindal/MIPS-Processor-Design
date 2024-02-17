879e59f120824e83f248f44003eb45aba8380853
IbrahimMI <141554753+IbrahimMI124@users.noreply.github.com>
2024-02-17 11:04:32 +0530

    Update Utilities.py

--- Utilities.py
+++ Utilities.py
@@ -1,10 +1,32 @@
 import sys
+from math import log2, ceil
+from enum import Enum
+
+class Opcode(Enum):
+    ADDI = "001000"
+    ADDIU = "001001"
+    BEQ = "000100"
+    BNE = "000101"
+    LUI = "001111"
+    LW = "100011"
+    ORI = "001101"
+    SLTI = "001010"
+    SW = "101011"
+    R_FORMAT = "000000"
+    J = "000010"
+    JAL = "000011"
+    JR = "000000"
+    DIV = "000000"
+    MFLO = "000000"
+    MFHI = "000000"
+    BREAK = "000000"
+    MUL = "011100"
 
 def typeCheck(typeMap: dict):
     '''
     'typeMap' must be a dictionary with key-value pairs as data and its type.
     The function returns true if all the keys match their dataTypes. It prints
-    an error message and exits otherwise.
+    an error and exits otherwise.
     '''
     if(not isinstance(typeMap, dict)):
         printErrorandExit(f"{typeMap} is not of type {type({})}")
@@ -17,13 +39,14 @@ def typeCheck(typeMap: dict):
 
 def printErrorandExit(message:str):
     '''
-    This function is for printing an error message and exit.
+    This function is for printing an error message and exiting.
     The message must be a string.
     '''
     if(not isinstance(message, str)):
         print(f"{message} is not of type {type('')}")
         sys.exit(1)
     print(message)
+    print("Program terminated.")
     sys.exit(1)
 
 if __name__=='__main__':
