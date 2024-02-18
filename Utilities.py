19f22780c33769fafadaa4ffc970150d62c7c5fa
IbrahimMI <141554753+IbrahimMI124@users.noreply.github.com>
2024-02-18 14:07:37 +0530

    Update Utilities.py

--- Utilities.py
+++ Utilities.py
@@ -1,6 +1,7 @@
 import sys
 from math import log2, ceil
 from enum import Enum
+from typing import Callable
 
 class Opcode(Enum):
     ADDI = "001000"
@@ -93,8 +94,10 @@ def printErrorandExit(message:str):
     print("Program terminated.")
     sys.exit(1)
 
+def func():
+    return 1
 if __name__=='__main__':
-    dic = {1: int, 2: int, 3: int}
+    dic = {1: int, 2: int, 3: int, 4: Callable}
     print(typeCheck(dic))
     print(typeCheck.__doc__)
     printErrorandExit(45)
