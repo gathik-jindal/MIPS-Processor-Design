8468c1c8b30758e4d2a6f782bcc453c9202133fc
Mohammed Ibrahim <141554753+IbrahimMI124@users.noreply.github.com>
2024-02-25 12:22:01 +0530

    Update ALU.py

--- ALU.py
+++ ALU.py
@@ -22,8 +22,7 @@ class ALU():
     self.__oPC = outPortCount
 
     self.__inpPorts = [ self._ground for i in range(self.__iPC)]        #Index 0: RD1; Index 1: ALUSrcMux; Index 2: shamt
-    self.__outPorts = [ self._ground for i in range(self.__oPC)]        #Index 0: Zero Flag; Index 1: ALUresult
-    self.__outValues = [self._ground() for i in range(self.__oPC)]
+    self.__outPorts = [ self._ground() for i in range(self.__oPC)]        #Index 0: Zero Flag; Index 1: ALUresult
 
     self.__operations = {
             Operation.NOP           :           self.__nop,
@@ -41,112 +40,119 @@ class ALU():
             Operation.MAG           :           self.__mag
     }
 
-    def _ground(self):
-      """
-          Used for grounding control signals
-      """
+  def _ground(self):
+    """
+        Used for grounding control signals
+    """
 
-      return 0
+    return 0
 
-    def run(self):
-      """
-          This method executes the operation based on the current control value.
-      """
-      self.__operation[self.__control()]()
+  def run(self):
+    """
+        This method executes the operation based on the current control value.
+    """
+    self.__operation[self.__control()]()
 
-    def __nop(self):
-      """
-          This method executes a NOP(does nothing).
-      """
-      pass
-
-
-    def __add(self):
-      """
-          Method for add operation.
-      """
-      self.__outPorts[1] = self.__inpPorts[0]()+self.__inpPorts[1]()
-      self.__outPorts[0] = ((self.__inpPorts[0]()+self.__inpPorts[1]() and 1)+1)%2
-
-    def __sll(self):
-      """
-          Method for shift left logical operation.
-      """
-
-      self.__outPorts[1] = self.__inpPorts[0]() << self.__inpPorts[2]()
-      self.__outPorts[0] = ((self.__inpPorts[0]() << self.__inpPorts[2]() and 1)+1)%2
-
-    def __sra(self):
-      """
-          Method for shift right arithmetic operation.
-      """
-      self.__outPorts[1] = self.__inpPorts[0]() >> self.__inpPorts[2]()
-      self.__outPorts[0] = ((self.__inpPorts[0]() >> self.__inpPorts[2]() and 1)+1)%2
-
-    def __sub(self):
-      """
-          Method for sub operation.
-      """
-      self.__outPorts[1] = self.__inpPorts[0]()-self.__inpPorts[1]()
-      self.__outPorts[0] = ((self.__inpPorts[0]()-self.__inpPorts[1]() and 1)+1)%2
-
-    def __comp(self):
-      """
-          Method for algebraic comparison of inputs.
-      """
-      self.__outPorts[1] = self.__inpPorts[0]()<self.__inpPorts[1]()
-      self.__outPorts[0] = ((self.__inpPorts[0]()-self.__inpPorts[1]() and 1)+1)%2
+  def __nop(self):
+    """
+        This method executes a NOP(does nothing).
+    """
+    pass
 
 
-    def __or(self):
-      """
-          Method for bitwise or operation.
-      """
-      self.__outPorts[1] = self.__inpPorts[0]() | self.__inpPorts[1]()
-      self.__outPorts[0] = ((self.__inpPorts[0]() | self.__inpPorts[1]() and 1)+1)%2
+  def __add(self):
+    """
+        Method for add operation.
+    """
+    self.__outPorts[1] = self.__inpPorts[0]()+self.__inpPorts[1]()
+    self.__outPorts[0] = ((self.__outPorts[1] and 1)+1)%2
 
-    def __xor(self):
-      """
-          Method for bitwise xor operation.
-      """
-      self.__outPorts[1] = self.__inpPorts[0]() ^ self.__inpPorts[1]()
-      self.__outPorts[0] = ((self.__inpPorts[0]() ^ self.__inpPorts[1]() and 1)+1)%2
+  def __sll(self):
+    """
+        Method for shift left logical operation.
+    """
 
-    def __div(self):
-      """
+    self.__outPorts[1] = self.__inpPorts[0]() << self.__inpPorts[2]()
+    self.__outPorts[0] = ((self.__outPorts[1] and 1)+1)%2
 
-      """
+  def __sra(self):
+    """
+        Method for shift right arithmetic operation.
+    """
+    self.__outPorts[1] = self.__inpPorts[0]() >> self.__inpPorts[2]()
+    self.__outPorts[0] = ((self.__outPorts[1] and 1)+1)%2
 
-      pass
+  def __sub(self):
+    """
+        Method for sub operation.
+    """
+    self.__outPorts[1] = self.__inpPorts[0]()-self.__inpPorts[1]()
+    self.__outPorts[0] = ((self.__outPorts[1] and 1)+1)%2
 
-    def __mul(self):
-      """
-      """
-
-      pass
-
-    def __ret(self):
-      """
-          Terminates the program.
-      """
-      print("Break instruction encountered.\nProgram Terminated.")
-      sys.exit(0)
-
-    def __mag(self):
-      """
-          This method handles all the syscall, mfho, mfli instructions.
-      """
-      pass
+  def __comp(self):
+    """
+        Method for algebraic comparison of inputs.
+    """
+    self.__outPorts[1] = self.__inpPorts[0]()<self.__inpPorts[1]()
+    self.__outPorts[0] = ((self.__outPorts[1] and 1)+1)%2
 
 
-    def setInputConnection(self, portID: int, portConnection:Callable):
-      typeCheck({portID : int, portConnection: Callable})
-      if(portID >= self.__iPC):
-        printErrorandExit(f"Invalid portID for an ALU with {self.__iPC} input ports.")
-      self.__inpPorts[portID] = portConnection
+  def __or(self):
+    """
+        Method for bitwise or operation.
+    """
+    self.__outPorts[1] = self.__inpPorts[0]() | self.__inpPorts[1]()
+    self.__outPorts[0] = ((self.__outPorts[1] and 1)+1)%2
 
-    def getOutput(self, portID: int):
-      typeCheck({portID: int})
-      if(portID >= self.__oPC):
-        printErrorandExit(f"Invalid portID for an ALU with {self.__oPC} output ports.")
-      return self.__outPorts[portID]
+  def __xor(self):
+    """
+        Method for bitwise xor operation.
+    """
+    self.__outPorts[1] = self.__inpPorts[0]() ^ self.__inpPorts[1]()
+    self.__outPorts[0] = ((self.__outPorts[1] and 1)+1)%2
+
+  def __div(self):
+    """
+
+    """
+
+    pass
+
+  def __mul(self):
+    """
+    """
+
+    pass
+
+  def __ret(self):
+    """
+        Terminates the program.
+    """
+    print("Break instruction encountered.\nProgram Terminated.")
+    sys.exit(0)
+
+  def __lui(self):
+    '''
+        Special operation to right shift immediate value by 16
+    '''
+    self.__outPorts[1] = self.__inpPorts[1] << 16
+    self.__outPorts[0] = ((self.__outPorts[1] and 1)+1)%2
+
+  def __mag(self):
+    """
+        This method handles all the syscall, mfho, mfli instructions.
+    """
+    pass
+
+
+  def setInputConnection(self, portID: int, portConnection:Callable):
+    typeCheck({portID : int, portConnection: Callable})
+    if(portID >= self.__iPC):
+      printErrorandExit(f"Invalid portID for an ALU with {self.__iPC} input ports.")
+    self.__inpPorts[portID] = portConnection
+
+  def getOutput(self, portID=1):
+    typeCheck({portID: int})
+    if(portID >= self.__oPC):
+      printErrorandExit(f"Invalid portID for an ALU with {self.__oPC} output ports.")
+    return self.__outPorts[portID]
