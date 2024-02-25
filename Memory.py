9a741e865ab350f5b9f51dc7a449d3bf5c8471ea
boom29 <Gathik.Jindal@iiitb.ac.in>
2024-02-25 14:32:42 +0530

    done

--- Memory.py
+++ Memory.py
@@ -8,6 +8,8 @@ STACKPOINTER = 0x7fffeffc
 GLOBALPOINTER = 0x10008000
 TEXT = 0x00400000
 
+MAXSTACKSIZE = 10000    # in bytes
+
 
 class Memory(ABC):
     """
@@ -31,12 +33,69 @@ class Memory(ABC):
         pass
 
     @abstractmethod
-    def storeWord(self, value: int, location: int) -> None:
+    def storeWord(self, value: str, location: int) -> None:
         """
         Stores the value at that memory location.
         """
 
         pass
+
+
+class DataMemory(Memory):
+    """
+    Handles which part of memory to access.
+    """
+
+    def __init__(self, readControl: Callable, writeControl: Callable, fileNameData: str, fileNameStack: str, fileNameGlobal: str) -> None:
+        """
+        Initializer
+
+        readControl: callable for the dataRead control signal
+        writeControl : callable for the dataWrite control signal
+        fileNameData: file name containing the .data part
+        fileNameStack: file name for containing stack
+        fileNameGlobal: file name for containing global (heap)
+        """
+        typeCheck({fileNameGlobal: str, fileNameData: str, fileNameStack: str,
+                  readControl: Callable, writeControl: Callable})
+
+        self.__fileNameGlobal = fileNameGlobal
+        self.__fileNameData = fileNameData
+        self.__fileNameStack = fileNameStack
+
+        self.__dataMem = Data(readControl, writeControl, self.__fileNameData)
+        self.__global = Global(self.__fileNameGlobal)
+        self.__stack = Stack(self.__fileNameStack)
+
+    def loadWord(self, location: int) -> str:
+        """
+        Loads the word into that memory location.
+        """
+        if (location < DATA):
+            return self.__global.loadWord(location)
+        else:
+            if (STACKPOINTER - location < MAXSTACKSIZE):
+                return self.__stack.loadWord(location)
+            else:
+                return self.__dataMem.loadWord(location)
+
+    def storeWord(self, value: str, location: int) -> None:
+        """
+        Store the word at that memory location.
+        """
+        if (location < DATA):
+            return self.__global.storeWord(value, location)
+        else:
+            if (STACKPOINTER - location < MAXSTACKSIZE):
+                return self.__stack.storeWord(value, location)
+            else:
+                return self.__dataMem.storeWord(value, location)
+
+    def loadString(self, location: int) -> str:
+        """
+        Reads the string until a null terminating character is read.
+        """
+        return self.__dataMem.loadString(location)
 
 
 class Global(Memory):
@@ -50,7 +109,7 @@ class Global(Memory):
         self.__gpWrtToFile = 0
         super().__init__(fileName)
 
-    def storeWord(self, value: int, location: int) -> None:
+    def storeWord(self, value: str, location: int) -> None:
         """
         Stores the word in the global heap.
         """
@@ -105,7 +164,7 @@ class Global(Memory):
         return word
 
 
-class DataMemory(Memory):
+class Data(Memory):
     """
     Handles/Contains strictly the .data part.
     Note file provided should be of .TXT format.
@@ -121,12 +180,19 @@ class DataMemory(Memory):
         self.__writeControl = writeControl
 
     def loadWord(self, location: int) -> str:
+        """
+        Loads the 32 bit word from the address
+        """
         if (self.__readControl() == 0):
             print("Read Enable is 0, cannot read word from DataMemory.")
             return ""
         return self.__loadWord(location)
 
-    def storeWord(self, value, location: int) -> str:
+    def storeWord(self, value: str, location: int) -> str:
+        """
+        Stores the word in the proceeding 32 bits / 4 memory location.
+        Location and value should be of type integer.
+        """
         if (self.__writeControl() == 0):
             print("Write Enable is 0, cannot write word to DataMemory.")
             return ""
@@ -204,7 +270,6 @@ class DataMemory(Memory):
     def __storeWord(self, value: str, location=DATA) -> None:
         """
         Stores the word in the proceeding 32 bits / 4 memory location.
-        In reality it is stored as 8 hexadecimal digits.
         Location and value should be of type integer.
         """
 
@@ -237,7 +302,7 @@ class Stack(Memory):
     def __init__(self, fileName="LinkedListStackBin.txt") -> None:
         super().__init__(fileName)
 
-    def storeWord(self, value: int, location: int) -> None:
+    def storeWord(self, value: str, location: int) -> None:
         """
         Stores the word in the stack.
         """
@@ -296,7 +361,7 @@ class InstructionMemory(Memory):
     def __init__(self, fileName="LinkedListTextBin") -> None:
         super().__init__(fileName)
 
-    def storeWord(self, value: int, location: int) -> None:
+    def storeWord(self, value: str, location: int) -> None:
         """
         Dummy class, will never be called.
         """
@@ -334,28 +399,35 @@ if __name__ == "__main__":
     # .data starts from 0x10010000 (DATA)
     # $sp starts from 0x7fffeffc (STACKPOINTER)
     # $gp starts from 0x10008000 (GLOBALPOINTER)
+    # DATA = 0x10010000
+    # STACKPOINTER = 0x7fffeffc
+    # GLOBALPOINTER = 0x10008000
+    # TEXT = 0x00400000
     value = "11111111111111111111111111111111"
 
-    obj = DataMemory(foo, foo, "LinkedListDataBin")
+    obj = DataMemory(foo, foo, "LinkedListDataBin",
+                     "LinkedListStackBin", "LinkedListHeapBin")
+
+    # data
     x = 4
     print(obj.loadWord(DATA + x))
     obj.storeWord(value, DATA + x)
     print(obj.loadWord(DATA + x))
     print(obj.loadString(DATA + 70))
 
-    obj = InstructionMemory("LinkedListTextBin")
-    x = 6
-    print(obj.loadWord(TEXT + x*4))
-
-    obj = Stack("LinkedListStackBin")
+    # stack
     x = 8
     obj.storeWord(value, STACKPOINTER - x)
     print(obj.loadWord(STACKPOINTER - x))
 
-    obj = Global("LinkedListHeapBin")
+    # global
     x = 8
     obj.storeWord(value, GLOBALPOINTER - x)
     print(obj.loadWord(GLOBALPOINTER - x))
     x = -8
     obj.storeWord(value, GLOBALPOINTER - x)
     print(obj.loadWord(GLOBALPOINTER - x))
+
+    obj = InstructionMemory("LinkedListTextBin")
+    x = 6
+    print(obj.loadWord(TEXT + x*4))
